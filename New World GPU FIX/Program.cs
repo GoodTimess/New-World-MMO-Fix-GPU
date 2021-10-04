22ec739140ced9b24f44ade9ad895e87267ffa68
GoodTimess <91896209+GoodTimess@users.noreply.github.com>
2021-10-04 16:03:01 +0200

    Add files via upload

---
+++ New World GPU FIX/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace New_World_GPU_FIX
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var appdatapath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var newworldpath = appdatapath + @"\Roaming\AGS\New World\user_preload_settings.cfg";
+            if (!File.Exists(newworldpath))
+            {
+                Console.WriteLine("Open the game once first!");
+            }
+            else
+            {
+                var text = File.ReadAllText(newworldpath);
+                var start = text.IndexOf("m_maxFps") + 9;
+                var finish = text.IndexOf("m_maxFps") + 11;
+                var correctconfig = text.Substring(0, start) + "60" + text.Substring(finish);
+                File.WriteAllText(newworldpath, correctconfig);
+            }
+        }
+    }
+}
