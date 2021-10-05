6341d592c1c68eaf087e165843b19d5c55f54e9d
Zenek <jacopo.menga@gmail.com>
2021-10-05 16:40:52 +0200

    some little changes, new comments better division of the code and many more

--- New World GPU FIX/Program.cs
+++ New World GPU FIX/Program.cs
@@ -9,14 +9,10 @@ namespace New_World_GPU_FIX
     {
         private static void Main(string[] args)
         {
-            string appdatapath = "Not found";
-            string newworldpath = "Not found";
-
             // Loading the main configuration (path folder ect..)
+            string newworldpath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Roaming\AGS\New World\user_preload_settings.cfg";
             Console.WriteLine("Loading necessary resources...");
-            appdatapath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            newworldpath = appdatapath + @"\Roaming\AGS\New World\user_preload_settings.cfg";
-            Debug.WriteLine($"Current path: {appdatapath}, \n\n New World path: {newworldpath}");
+            Debug.WriteLine($">>> New World path: {newworldpath}");
             Thread.Sleep(1);
 
             // Looking for the existing file
@@ -24,22 +20,30 @@ namespace New_World_GPU_FIX
             if (!File.Exists(newworldpath))
             {
                 Console.WriteLine("Open the game once first!");
+                return;
             }
             else
             {
                 Console.WriteLine("Injecting correct configuration...");
+                Thread.Sleep(1);
                 try
                 {
-                    var text = File.ReadAllText(newworldpath);
+                    // Try to find the user preload settings file
+                    string text = File.ReadAllText(newworldpath);
+
+                    // Looking for a specific declaration and perform some changes
                     var start = text.IndexOf("m_maxFps") + 9;
                     var finish = text.IndexOf("m_maxFps") + 11;
                     var correctconfig = text.Substring(0, start) + "60" + text.Substring(finish);
+
+                    // Write down the changes and save the new configuration
                     File.WriteAllText(newworldpath, correctconfig);
                     Console.WriteLine("All done! Await for exit...");
                     Thread.Sleep(1);
                 }
                 catch (Exception ex)
                 {
+                    // If there are any errors, they should be captured by this catch and reported in the console
                     Console.WriteLine($"Error: {ex}");
                 }
             }
--- README.md
+++ README.md
@@ -2,14 +2,19 @@
 
 New World GPU Burning Fix: This mod allow you to cap the max fps in order to limit the GPU Workload where is unnecessary and mitigate the gpu-burning event.
 
-## About
+-----------
+
+# About
 Info | Description
 --- | ---
 `Language` | *C#*
 `Developer` | *GoodTimess*
 `Base Version` | *1.0*
 
+-----------
+
 # Change Logs
 
 ## Version 1.0 (04/10/2021)
 -[x] First release
+
